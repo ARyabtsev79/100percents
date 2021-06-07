08bcc48895052e350f10eb4ac9229c4edbb4bc9f
ARyabtsev79 <ryabtsev79@gmail.com>
2021-06-07 12:19:27 +0300

    add test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,15 @@ class StatisticsServiceTest {
 
     assertEquals(expected, actual);
   }
+  @Test
+  void shouldfindMaxSeven() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {12, 5, 8, 4, 5, 15, 23, 6, 11, 11, 12};
+    long expected = 23;
+
+    long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
 }
